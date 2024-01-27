129d4360e502823c159c8840db83e95c3fefe9ee
Roma.code <romadani.code@gmail.com>
2024-01-28 03:16:09 +0700

    soal Dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\SoalController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +17,26 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.auth.login');
 });
+
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('home', function () {
+        return view('pages.dashboard');
+    })->name('home');
+    Route::resource('user', UserController::class);
+    Route::resource('soal', SoalController::class);
+});
+
+// Route::get('/login', function () {
+//     return view('pages.auth.login');
+// })->name('login');
+
+// Route::get('/register', function () {
+//     return view('pages.auth.register');
+// })->name('register');
+
+// Route::get('/users', function () {
+//     return view('pages.users.index');
+// });
